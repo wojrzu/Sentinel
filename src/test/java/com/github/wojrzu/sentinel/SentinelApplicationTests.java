04b4456fbbb0fc9e48983f51a7ba7393dd17d425
SimpleBadger <wojciechrzucidlo22@gmail.com>
2026-03-15 13:58:35 +0100

    Initial commit

---
+++ src/test/java/com/github/wojrzu/sentinel/SentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wojrzu.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
